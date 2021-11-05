a977dabcedb8c7983fee6403b2cb36f481d12e22
drew1701 <drew@drewpatrick.org>
2021-11-05 16:50:53 -0700

    start repo
    assignment files from cnit133m, mobile web programming class

---
+++ st1/service-worker.js
@@ -0,0 +1,69 @@
+// Names of the two caches used in this version of the service worker.
+// Change to v2, etc. when you update any of the local resources, which will
+// in turn trigger the install event again.
+const PRECACHE = 'cnit133m_st1_v1.0';
+const RUNTIME = 'runtime';
+
+const PRECACHE_URLS = [
+    'index.html',
+    './',
+    'bootstrap.min.css',
+    'jquery-3.5.1.slim.min.js',
+    'bootstrap.bundle.min.js',
+    'ionic-logo_300x300t.png',
+    'react-logo_300x300t.png',
+    'flutter-logo_300x300t.png',
+    'icon/favicon-32x32.png',
+    'icon/favicon-16x16.png',
+    'icon/favicon-96x96.png',
+    'icon/android-icon-192x192.png',
+    'icon/android-icon-512x512.png',
+];
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics if you have one.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
